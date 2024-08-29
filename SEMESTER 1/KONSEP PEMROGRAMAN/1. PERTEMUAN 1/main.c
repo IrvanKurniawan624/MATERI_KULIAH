cbe488055aa92a2f71f56e9c52d8966ad4e326f3
IrvanKurniawan <irvankurniawan624@gmail.com>
2024-08-29 12:45:00 +0700

    commit 29/08/2024

---
+++ SEMESTER 1/KONSEP PEMROGRAMAN/1. PERTEMUAN 1/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//    NOTE : UNTUK COMMENT MULTIPLE LINE GUNAKAN CTRL + SHIFT + C
+//    NOTE : UNTUK UNCOMMENT MULTIPLE LINE GUNAKAN CTRL + SHIFT + X
+
+
+//    START TUGAS 1
+//    printf("NAMA : IRVAN ADITYA KURNIAWAN");
+//    printf("NRP : 3124600044");
+//    END TUGAS 1
+
+//    START TUGAS 2
+//    printf("PERHITUNGAN PENJUMLAHAN 1024 + 4096 = %d", 1024+4096);
+//    END TUGAS 2
+
+//    START TUGAS 3
+    int nilai1, nilai2;
+    printf("PROGRAM PERHITUNGAN PENJUMLAHAN\n");
+    printf("Masukkan Nilai 1: ");
+    scanf("%d", &nilai1);
+    printf("Masukkan Nilai 2: ");
+    scanf("%d", &nilai2);
+    printf("HASIL PENJUMLAHAN %d + %d = %d", nilai1, nilai2, nilai1+nilai2);
+//    END TUGAS 3
+
+//    START TUGAS 4
+//    float nilai1, hasil;
+//    printf("MASUKKAN NILAI FLOAT : ");
+//    scanf("%f", &nilai1);
+//    hasil = nilai1 * 50;//    printf("MASUKKAN NILAI FLOAT : ");
+//    scanf("%f", &nilai1);
+//    printf("HASIL DARI FLOAT %f X 50 = %f", nilai1, hasil);
+//    END TUGAS 4
+
+//    START TUGAS 5
+//    float nilai;
+//    printf("FLOAT = %s", nilai);
+//    END TUGAS 5
+
+//    START TUGAS 6
+//    int nilai1 = 50;
+//    char nilai2 = 'A';
+//    printf("NILAI INTEGER = %d dan NILAI CHAR = %c", nilai1, nilai2);
+//    END TUGAS 6
+
+
+    return 0;
+}
---
+++ SEMESTER 1/KONSEP PEMROGRAMAN/2. PERTEMUAN 2/TUGAS PENDAHULUAN PRAKTIKUM 3.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+///    TUGAS PENDAHULUAN PRAKTIKUM 3
+
+///    =======TUGAS 1=======
+//    int nominal_dollar, konversi_rupiah;
+//    printf("Masukkan Jumlah Uang Dollar : ");
+//    scanf("%d", &nominal_dollar);
+//    konversi_rupiah = nominal_dollar * 11090;
+//    printf("Konversi ke Rupiah : %d", konversi_rupiah);
+///    =======END=======
+
+///    =======TUGAS 2=======
+//    int jml_uang, ratusan_ribu, lima_plh_ribu, dua_plh_ribu, sepuluh_ribu, lima_ribu, dua_ribu, seribu, sisa;
+//    printf("Masukkan Uang Rupiah : ");
+//    scanf("%d", &jml_uang);
+//    ratusan_ribu = jml_uang / 100000;
+//    sisa = jml_uang - (ratusan_ribu*100000);
+//    lima_plh_ribu = sisa / 50000;
+//    sisa = sisa - (lima_plh_ribu * 50000);
+//    dua_plh_ribu = sisa / 20000;
+//    sisa = sisa - (dua_plh_ribu * 20000);
+//    sepuluh_ribu = sisa / 10000;
+//    sisa = sisa - (sepuluh_ribu * 10000);
+//    lima_ribu = sisa / 5000;
+//    sisa = sisa - (lima_ribu* 5000);
+//    dua_ribu = sisa / 2000;
+//    sisa = sisa - (dua_ribu* 2000);
+//    seribu = sisa / 1000;
+//    sisa = sisa - (seribu * 1000);
+//
+//    printf("Output : \n %d Lembar 100.000 \n %d Lembar 50.000 \n %d Lembar 20.000 \n %d Lembar 10.000 \n %d Lembar 5000\n %d Lembar 2000\n %d Lembar 1000", ratusan_ribu, lima_plh_ribu, dua_plh_ribu, sepuluh_ribu, lima_ribu, dua_ribu, seribu);
+///    =======END=======
+
+///    =======TUGAS 3=======
+//    int a = 12, b = 2, c = 3, d = 4;
+//    printf("Nilai a = %d, b = %d, dan c = %d\n", a, b, c);
+//    printf("a %% b = %d\n", a % b);
+//    printf("a - c = %d\n", a - c);
+//    printf("a + b = %d\n", a + b);
+//    printf("a / d = %d\n", a / d);
+//    printf("a / d * d + a %% d = %d\n", a / d * d + a % d);
+//    printf("a %% d / d * a - c = %d\n", a % d / d * a - c);
+///    =======END=======
+
+///    =======TUGAS 4=======
+//    int a, b, c, hasil;
+//    printf("Masukkan Nilai A, B, dan C (pisahkan dengan spasi) = ");
+//    scanf("%d %d %d", &a, &b, &c);
+//    hasil = (b * b) - 4 * a * c;
+//    printf("Hasil Diskriman : %d", hasil);
+///    =======END=======
+
+///    =======TUGAS 5=======
+//    float a, b, c, hasil;
+//    printf("Masukkan Nilai A, B, dan C (pisahkan dengan spasi) = ");
+//    scanf("%f %f %f", &a, &b, &c);
+//    hasil = (b * b) - 4 * a * c;
+//    printf("Hasil Diskriman (FLOAT) : %.2f", hasil);
+///    =======END=======
+
+
+    return 0;
+}
